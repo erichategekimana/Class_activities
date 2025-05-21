dd4685782a97e73a50337dccab39d0d6f75feea2
root <root@DESKTOP-8VNRNUT>
2025-05-21 10:55:57 +0200

    script push

---
+++ first_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+welcome="First shell Script"
+echo "$welcome !"
+ # A Simple script to greet the user
+ echo "What is your name?"
+ read name
+ echo "Hello, $name! Welcome to shell scripting."
+
